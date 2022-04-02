1e1f8f01ed73a6dd0282813442ea20ebaa8f5028
Paco Torrado <developer@pacotorrado.com>
2022-04-02 13:00:13 +0200

    Rename test

--- src/test/java/org/pacotorrado/algorithms/RansomNoteCheckerTest.java
+++ src/test/java/org/pacotorrado/algorithms/RansomNoteCheckerTest.java
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-public class RansomNoteTest {
+public class RansomNoteCheckerTest {
     RansomNoteChecker ransomNoteChecker = new RansomNoteChecker();
 
     @Test
