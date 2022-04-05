1a8456226172953d196ea16555f21fd59428d2fa
Paco Torrado <developer@pacotorrado.com>
2022-04-05 22:51:09 +0200

    Richest Customer Wealth

--- src/main/java/org/pacotorrado/algorithms/RichestCustomerWealth.java
+++ src/main/java/org/pacotorrado/algorithms/RichestCustomerWealth.java
@@ -2,4 +2,18 @@ package org.pacotorrado.algorithms;
 
 public class RichestCustomerWealth {
 
+    int maximumWealth(final int[][] accounts) {
+        int maximum = 0;
+        int accountsNumber = accounts[0].length;
+        for (int i = 0; i < accounts.length; i++) {
+            int amount = 0;
+            for (int j = 0; j < accountsNumber; j++) {
+                amount += accounts[i][j];
+            }
+            if (amount > maximum) {
+                maximum = amount;
+            }
+        }
+        return maximum;
+    }
 }
--- src/test/java/org/pacotorrado/algorithms/RichestCustomerWealthTest.java
+++ src/test/java/org/pacotorrado/algorithms/RichestCustomerWealthTest.java
@@ -1,7 +1,30 @@
 package org.pacotorrado.algorithms;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class RichestCustomerWealthTest {
+
+    RichestCustomerWealth richestCustomerWealth = new RichestCustomerWealth();
+
+    @Test
+    void shouldReturnValidAmount_6() {
+        int[][] accounts = {{1, 2, 3}, {3, 2, 1}};
+        assertEquals(6, richestCustomerWealth.maximumWealth(accounts));
+    }
+
+    @Test
+    void shouldReturnValidAmount_10() {
+        int[][] accounts = {{1, 5}, {7, 3}, {3, 5}};
+        assertEquals(10, richestCustomerWealth.maximumWealth(accounts));
+    }
+
+
+    @Test
+    void shouldReturnValidAmount_17() {
+        int[][] accounts = {{2, 8, 7}, {7, 1, 3}, {1, 9, 5}};
+        assertEquals(17, richestCustomerWealth.maximumWealth(accounts));
+    }
 
 }
